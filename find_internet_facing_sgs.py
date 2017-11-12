b406ff92e6dea4002a3dc931018874eaee2c82eb
guddu07 <p2006.prashant@gmail.com>
2017-11-12 09:30:19 +0530

    Internet facing security groups

--- find_internet_facing_sgs.py
+++ find_internet_facing_sgs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
-import boto3
-ec2 = boto3.resource('ec2', region_name='ap-southeast-2')
-sgs = ec2.security_groups.all()
-security_groups = [sg.id for sg in sgs]
-for security_group in security_groups:
-    sg = ec2.SecurityGroup(security_group)
-    for i in range(len(sg.ip_permissions)):
-        for j in range(len(sg.ip_permissions[i]['IpRanges'])):
+import boto3 # import boto3 library 
+
+ec2 = boto3.resource('ec2', region_name='ap-southeast-2') # Initialize ec2 object 
+
+sgs = ec2.security_groups.all() # Fetching all security groups from account
+
+security_groups = [sg.id for sg in sgs] # Capturing all security groups in a list to avoid calling again and again
+
+for security_group in security_groups: # Initialize for loop to go inside each security group to capture details
+    sg = ec2.SecurityGroup(security_group) # Capture individual security group details
+    for i in range(len(sg.ip_permissions)): # Capturing ingress rules only
+        for j in range(len(sg.ip_permissions[i]['IpRanges'])): # Initializing nested loop to iterate over rules inside a security group
             if "0.0.0.0/0" in sg.ip_permissions[i]['IpRanges'][j]['CidrIp']:
                 print(sg.group_name, sg.ip_permissions[i]['ToPort'], sg.ip_permissions[i]['IpRanges'][j]['CidrIp'])
